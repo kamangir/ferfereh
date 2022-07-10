e004898d3a2cbd48b6137c5a025f2f4c140d675a
kamangir <arash@kamangir.net>
2022-07-10 11:27:05 -0400

    update - kamangir/todo#135

--- abcli/ferfereh.sh
+++ abcli/ferfereh.sh
@@ -6,11 +6,6 @@ function ferfereh() {
     if [ $task == "help" ] ; then
         abcli_help_line "ferfereh publish" \
             "publish ferfereh."
-
-        if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
-            python3 -m ferfereh --help
-        fi
-
         return
     fi
 
--- ferfereh/__init__.py
+++ ferfereh/__init__.py
@@ -1,5 +1,5 @@
 name = "ferfereh"
 
-version = "1.1.29"
+version = "1.1.31"
 
 description = "3d-printed graffiti"

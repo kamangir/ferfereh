f65cd0cb6c40213544f3c60aa3651fd363464030
kamangir <arash@kamangir.net>
2024-05-23 19:05:39 -0700

    refactor - kamangir/bolt#746

--- .abcli/ferfereh.sh
+++ .abcli/ferfereh.sh
@@ -1,9 +1,5 @@
 #! /usr/bin/env bash
 
-function F() {
-    ferfereh "$@"
-}
-
 function ferfereh() {
     local task=$(abcli_unpack_keyword $1 help)
 
@@ -17,43 +13,15 @@ function ferfereh() {
         return
     fi
 
-    local function_name="ferfereh_$task"
-    if [[ $(type -t $function_name) == "function" ]]; then
-        $function_name "${@:2}"
-        return
-    fi
-
     if [ "$task" == "cleanup" ]; then
         python3 -m ferfereh cleanup \
             --path $abcli_path_git/ferfereh/images/tools
         return
     fi
 
-    if [ "$task" == "init" ]; then
-        abcli_init ferfereh "${@:2}"
-        return
-    fi
-
-    if [[ "|pylint|pytest|test|" == *"|$task|"* ]]; then
-        abcli_${task} plugin=ferfereh,$2 \
-            "${@:3}"
-        return
-    fi
-
-    if [[ "|pypi|" == *"|$task|"* ]]; then
-        abcli_${task} "$2" \
-            plugin=ferfereh,$3 \
-            "${@:4}"
-        return
-    fi
-
-    if [ "$task" == "version" ]; then
-        python3 -m ferfereh version "${@:2}"
-        return
-    fi
-
-    abcli_log_error "-ferfereh: $task: command not found."
-    return 1
+    abcli_generic_task \
+        plugin=ferfereh,task=$task \
+        "${@:2}"
 }
 
 abcli_source_path \
--- ferfereh/__init__.py
+++ ferfereh/__init__.py
@@ -4,4 +4,4 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} 3d-printed graffiti"
 
-VERSION = "1.125.1"
+VERSION = "1.126.1"

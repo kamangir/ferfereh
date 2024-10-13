679b7b6bcd0b0d869b245680b3c10a144ce6740e
kamangir <arash@kamangir.net>
2024-10-13 13:08:00 -0700

    git refactor - kamangir/bolt#746

--- ferfereh/.abcli/actions.sh
+++ ferfereh/.abcli/actions.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
 function ferfereh_action_git_before_push() {
-    [[ "$(abcli_git get_branch)" == "main" ]] &&
-        ferfereh pypi build
+    [[ "$(abcli_git get_branch)" != "main" ]] &&
+        return 0
+
+    ferfereh pypi build
 }
